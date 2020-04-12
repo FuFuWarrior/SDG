f04c1d17e3eac02e855e604aca7f2c9eeee7c6fd
FuFuWarrior <akpuwarrior@gmail.com>
2020-04-12 11:42:06 +0100

    added status code 201

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ app.use(express.json());
 app.post('/api/v1/on-covid-19', (req, res) => {
   const data = req.body;
   const result = estimate(data);
-  res.send(result).status(200);
+  res.send(result).status(201);
 });
 
 app.post('/api/v1/on-covid-19/json', (req, res) => {
   const data = req.body;
   const result = estimate(data);
-  res.send(result).status(200);
+  res.send(result).status(201);
 });
 
 
@@ -25,7 +25,7 @@ app.post('/api/v1/on-covid-19/xml', (req, res) => {
   const data = req.body;
   const builder = new xml2js.Builder();
   const xml = builder.buildObject(estimate(data));
-  res.send(xml).status(200);
+  res.send(xml).status(201);
 });
 
 module.exports = app;
