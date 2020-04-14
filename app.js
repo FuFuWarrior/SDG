0e308a0097883adaf09ce38672a0c02c7317a9bf
FuFuWarrior <akpuwarrior@gmail.com>
2020-04-14 16:17:32 +0100

    fix logic

--- app.js
+++ app.js
@@ -48,11 +48,13 @@ app.post('/api/v1/on-covid-19/xml', (req, res) => {
   const builder = new xml2js.Builder();
   const xml = builder.buildObject(estimate(data));
   res.header('Content-Type', 'application/xml; charset=UTF-8')
+  logger(req,res);
   return res.status(201).send(xml);
 });
 
 app.get('/api/v1/on-covid-19/logs', (req, res, next) => {
   const logs = fs.readFileSync('./logs.txt');
+  logger(req,res);
   res.send(logs).status(200);
 });
 module.exports = app;
